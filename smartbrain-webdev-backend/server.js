3d309b3139f9ac7526167dc2d2d7f6d0587a3761
Divya Dadarya <divyadadarya@gmail.com>
2020-08-10 18:57:29 +0530

    port change

--- smartbrain-webdev-backend/server.js
+++ smartbrain-webdev-backend/server.js
@@ -88,4 +88,4 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 // });
 
 
-app.listen(5000, () => console.log('app listening on port 5000!'))
+app.listen(process.env.PORT || 5000, () => console.log(`app listening on port ${process.env.PORT}!`))
